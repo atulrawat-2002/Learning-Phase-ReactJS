42e6f47bbec3732527778da2015563326c6e819f
Atul Rawat <dr6217811@gmail.com>
2025-04-27 09:23:22 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,53 @@
+/*****
+ * 
+ * 
+ * <div>
+ *  <div id="child">
+ *      <h1> I am an h1 tag </h1>
+ *      <h2> I am an h1 tag </h2>
+ *  </div?
+ *  <div id="child">
+ *      <h1> I am an h1 tag </h1>
+ *      <h2> I am an h1 tag </h2>
+ *  </div?
+ * </div>
+ * 
+ * ******/
+
+
+
+let parent = React.createElement(
+    "div",
+    { id: "parent", },
+    [
+        React.createElement("div",
+            { id: "child1", },
+            [
+                React.createElement("h1",
+                    {},
+                    "I am h1 tag"),
+                React.createElement("h2",
+                    {},
+                    "I am h2 tag")
+            ]),
+        React.createElement("div",
+            { id: "child2", },
+            [
+                React.createElement("h1",
+                    {},
+                    "I am h1 tag"),
+                React.createElement("h2",
+                    {},
+                    "I am h2 tag")
+            ])
+    ]
+)
+
+
+
+
+console.log(parent)
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent)
